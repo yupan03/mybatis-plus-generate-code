39b768e31fc58e2a3ee3c1bf77af2abce1f04c1a
yupan03 <735997736@qq.com>
2019-11-08 08:44:57 +0800

    创建项目

---
+++ src/main/java/com/pp/Config.java
@@ -0,0 +1,21 @@
+package com.pp;
+
+public class Config {
+    // 数据库配置 start
+    private String url;
+
+    private String dataBase;
+
+    private String username;
+
+    private String password;
+    // 数据库配置 end
+
+    // 项目路径
+    private String projectPath;
+    // 父包名
+    private String packageName;
+    
+    // xml文件路径
+    private String xmlPath;
+}
